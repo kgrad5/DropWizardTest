e1e0ffa38b7abef1c07ebb89b137cd0f254ff1ba
Kevin Grad <kgrad5@gmail.com>
2013-05-04 23:39:31 -0400

    Added the new packages... whoops

---
+++ src/main/java/com/kgrad/dropwizardtest/configurations/ListDirectoryConfiguration.java
@@ -0,0 +1,15 @@
+package com.kgrad.dropwizardtest.configurations;
+
+import com.yammer.dropwizard.config.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class ListDirectoryConfiguration extends Configuration {
+    @NotEmpty
+    @JsonProperty
+    private String directory;
+
+    public String getDirectory() {
+        return directory;
+    }
+}
---
+++ src/main/java/com/kgrad/dropwizardtest/services/ListDirectoryService.java
@@ -0,0 +1,33 @@
+package com.kgrad.dropwizardtest.services;
+
+import com.kgrad.dropwizardtest.configurations.ListDirectoryConfiguration;
+import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Environment;
+import com.kgrad.dropwizardtest.resources.ListDirectoryResource;
+import com.kgrad.dropwizardtest.health.DirectoryHealthCheck;
+import com.kgrad.dropwizardtest.resources.DirectoryResource;
+import com.yammer.dropwizard.views.View;
+import com.yammer.dropwizard.views.ViewBundle;
+
+public class ListDirectoryService extends Service<ListDirectoryConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new ListDirectoryService().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<ListDirectoryConfiguration> bootstrap) {
+        bootstrap.setName("list-directory");
+        bootstrap.addBundle(new ViewBundle());
+    }
+
+    @Override
+    public void run(ListDirectoryConfiguration configuration,
+            Environment environment) {
+        final String directory = configuration.getDirectory();
+        environment.addResource(new ListDirectoryResource(directory));
+        environment.addResource(new DirectoryResource(directory));
+        environment.addHealthCheck(new DirectoryHealthCheck(directory));
+    }
+}
